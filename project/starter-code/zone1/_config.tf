54be8dc1bfa244c13780360db53e342452db6da3
metan.ch4 <131370461+Metan-CH4@users.noreply.github.com>
2024-11-26 15:41:08 +0700

    Update _config.tf

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-<user_name>"
+     bucket = "udacity-tf-nghile3"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
