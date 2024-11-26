2aeffc4184fb087b5be9c2c4ccbea3668bbdd6d8
metan.ch4 <131370461+Metan-CH4@users.noreply.github.com>
2024-11-26 16:07:39 +0700

    Update rds.tf

--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -2,7 +2,7 @@ variable primary_db_cluster_arn {}
 
 resource "aws_rds_cluster_parameter_group" "cluster_pg-s" {
   name   = "udacity-pg-s"
-  family = "aurora5.6"
+  family = "aurora-mysql5.7"
 
   parameter {
     name  = "binlog_format"    
@@ -29,18 +29,23 @@ resource "aws_rds_cluster" "udacity_cluster-s" {
   vpc_security_group_ids   = [aws_security_group.db_sg_2.id]
   db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
   engine_mode              = "provisioned"
-  engine_version           = "5.6.mysql_aurora.1.19.1" 
+  engine_version           = "5.7.mysql_aurora.2.11.1" 
+  source_region            = "us-east-2"
+  backup_retention_period  = 5
   skip_final_snapshot      = true
   storage_encrypted        = false
+  replication_source_identifier   = var.primary_db_cluster_arn
   depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
 }
 
 resource "aws_rds_cluster_instance" "udacity_instance-s" {
-  count                = 1
+  count                = 2
+  engine               = "aurora-mysql"
   identifier           = "udacity-db-instance-${count.index}-s"
   cluster_identifier   = aws_rds_cluster.udacity_cluster-s.id
   instance_class       = "db.t2.small"
   db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
+  depends_on           = [aws_rds_cluster_parameter_group.cluster_pg-s]
 }
 
 resource "aws_security_group" "db_sg_2" {
