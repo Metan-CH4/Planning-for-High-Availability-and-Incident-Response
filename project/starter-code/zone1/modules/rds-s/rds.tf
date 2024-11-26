7384ef063605e8456236b51d9ae9c3ba935bc152
metan.ch4 <131370461+Metan-CH4@users.noreply.github.com>
2024-11-27 04:16:00 +0700

    Update rds.tf

--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -23,20 +23,19 @@ resource "aws_db_subnet_group" "udacity_db_subnet_group" {
 }
 
 resource "aws_rds_cluster" "udacity_cluster-s" {
-  cluster_identifier       = "udacity-db-cluster-s"
-  availability_zones       = ["us-west-1a", "us-west-1c"]
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg-s.name
-  vpc_security_group_ids   = [aws_security_group.db_sg_2.id]
-  db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
-  engine                   = "aurora-mysql"
-  engine_mode              = "provisioned"
-  engine_version           = "5.7.mysql_aurora.2.11.2" 
-  source_region            = "us-east-2"
-  backup_retention_period  = 5
-  skip_final_snapshot      = true
-  storage_encrypted        = false
+  cluster_identifier              = "udacity-db-cluster-s"
+  availability_zones              = ["us-west-1a", "us-west-1c"]
+  vpc_security_group_ids          = [aws_security_group.db_sg_2.id]
+  db_subnet_group_name            = aws_db_subnet_group.udacity_db_subnet_group.name
+  engine_mode                     = "provisioned"
+  engine                          = "aurora-mysql"
+  engine_version                  = "5.7.mysql_aurora.2.11.2"
+  backup_retention_period         = 5
+  skip_final_snapshot             = true
+  storage_encrypted               = false
   replication_source_identifier   = var.primary_db_cluster_arn
-  depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
+  source_region                   = "us-east-2"
+  depends_on                      = [aws_rds_cluster_parameter_group.cluster_pg-s]
 }
 
 resource "aws_rds_cluster_instance" "udacity_instance-s" {
